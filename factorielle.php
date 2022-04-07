61b15ab3dbe124f768e1f526a16d307c2a1692b7
Alexandre Cuneo <alexandre.cuneo@gmail.com>
2022-04-07 12:00:56 +0200

    commit-alex

--- factorielle.php
+++ factorielle.php
@@ -7,3 +7,20 @@ function factorielle($fact)
     $fact = gmp_fact($fact);
     return $fact;
 }
+
+//FONCTION DE ALEX
+
+function nbEntier()
+{
+    do {
+        $tmp = readline("Entez un nombre entier plus grand que 0 >>");
+    } while (!ctype_digit($tmp));
+
+    if ($tmp <= 1) {
+        return 1;
+    } else {
+        return $tmp;
+    }
+}
+
+echo nbEntier();
